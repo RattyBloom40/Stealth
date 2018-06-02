dd18b2ecae1432c3a138674c78503e607ffad001
Ethan B <e10benga@gmail.com>
2018-06-02 11:03:40 -0500

    horizontal mouse look

---
+++ Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform cameraTarget;
+	public Transform cameraHolder;
+	public float snappiness = 1;
+
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		transform.LookAt(cameraTarget);
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition( Vector3.Lerp( transform.position, cameraHolder.position, snappiness ) );
+	}
+}
---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public static PlayerController instance;
+
+	public GameObject cameraTarget;
+	public float turnSpeed;
+
+	Animator anim;
+	Rigidbody rb;
+
+	public Transform cam;
+
+	public float speed;
+
+	void Start() {
+		if(instance == null)
+			instance = this;
+		else {
+			Debug.LogError( "2 PlayerControllers active" );
+			Destroy( gameObject );
+		}
+		anim = GetComponentInChildren<Animator>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		transform.RotateAround( transform.position, transform.up, Time.deltaTime * turnSpeed * Input.GetAxis( "Mouse X" ) );
+	}
+
+	void FixedUpdate() {
+		Vector3 input = Vector3.forward * Input.GetAxis( "Vertical" ) + Vector3.right * Input.GetAxis( "Horizontal" );
+		anim.SetFloat( "x input", Input.GetAxis( "Horizontal" ) );
+		anim.SetFloat( "z input", Input.GetAxis( "Vertical" ) );
+		rb.velocity = input * speed;
+	}
+}
